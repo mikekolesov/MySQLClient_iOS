0f1c7de8a0497897dff529e014674d873786104d
Michael Kolesov <michael@Michaels-MacBook-Pro.local>
2013-06-22 17:27:28 +0400

    Define fixed

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -x
 # Build Release versions of mysqllib. Change to "-configuration Debug" to get debug one
 
 cd mysql-connector-c-6.0.2
-cmake -G Xcode -DSKIP_SLL=1
+cmake -G Xcode -DSKIP_SSL=1
 xcodebuild -project libmysql.xcodeproj -target mysqlclient -configuration Release -sdk iphonesimulator6.1 ONLY_ACTIVE_ARCH=NO ARCHS=i386 PRODUCT_NAME=mysqlclient_simulator
 xcodebuild -project libmysql.xcodeproj -target mysqlclient -configuration Release -sdk iphoneos6.1 ONLY_ACTIVE_ARCH=NO ARCHS=armv7s PRODUCT_NAME=mysqlclient_device_armv7s
 xcodebuild -project libmysql.xcodeproj -target mysqlclient -configuration Release -sdk iphoneos6.1 ONLY_ACTIVE_ARCH=NO ARCHS=armv7 PRODUCT_NAME=mysqlclient_device_armv7
